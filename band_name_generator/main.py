3b52ec1a7073889238d1800d82049131a5539dfa
Khuslen98 <khuslenu.tamiraa@gmail.com>
2024-01-02 17:27:24 +0800

    13 hurtel uzlee

--- band_name_generator/main.py
+++ band_name_generator/main.py
@@ -1,2 +1,9 @@
 age = 16
 print (age)
+
+print("Day 1 - Python Print Function") 
+print("The function is declared like this:") 
+print("print('what to print')")
+
+print("Hello World!\nHello WORLD!\nHello World!\nHello World!\nHello World!\nHello World!\nHello World!\n")
+print("Hello" + " " +"Khuslen")
